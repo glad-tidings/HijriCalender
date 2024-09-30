3fbde7dab362e69984f5e65bf9c19e32dd95dfbc
Glad Tidings <37252168+glad-tidings@users.noreply.github.com>
2024-09-30 04:10:02 +0330

    Add files via upload

---
+++ ParsElecom.HijriCalender/Properties/AssemblyInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Web.UI;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pars Elecom Hijri Calender")]
+[assembly: AssemblyDescription("Pars Elecom Hijri Calender")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pars Elecom")]
+[assembly: AssemblyProduct("Pars Elecom Hijri Calender")]
+[assembly: AssemblyCopyright("Copyright © Pars Elecom 2009. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b467691-0726-4273-8afa-211e41d0b623")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+
+[assembly: WebResource("ParsElecom.HijriCalender.Javascript.datepicker.js", "application/x-javascript")]
+
+[assembly: WebResource("ParsElecom.HijriCalender.Style.theme.css", "text/css", PerformSubstitution = true)]
+
+[assembly: WebResource("ParsElecom.HijriCalender.Style.active-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.dark-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.hover-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.menuarrow.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.normal-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.rowhover-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.status-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.title-bg.gif", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Style.today-bg.gif", "image/png")]
+
+[assembly: WebResource("ParsElecom.HijriCalender.CalendarToolbox.bmp", "image/png")]
+[assembly: WebResource("ParsElecom.HijriCalender.Calendar.gif", "image/png")]
+
+[assembly: TagPrefix("ParsElecom.HijriCalender", "pec")]
