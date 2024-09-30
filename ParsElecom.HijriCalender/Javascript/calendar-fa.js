cf6ab331dc11039a1f78c7d00a858f25b031c96e
Glad Tidings <37252168+glad-tidings@users.noreply.github.com>
2024-09-30 04:05:52 +0330

    Add files via upload

---
+++ ParsElecom.HijriCalender/Javascript/calendar-fa.js
@@ -0,0 +1,113 @@
+Calendar._DN = new Array
+("یکشنبه",
+ "دوشنبه",
+ "سه شنبه",
+ "چهارشنبه",
+ "پنجشنبه",
+ "جمعه",
+ "شنبه",
+ "یکشنبه");
+
+Calendar._SDN = new Array
+("یک",
+ "دو",
+ "سه",
+ "چهار",
+ "پنج",
+ "جمعه",
+ "شنبه",
+ "یک");
+
+Calendar._FD = 6;
+
+Calendar._MN = new Array
+("ژانویه",
+ "فوریه",
+ "مارس",
+ "آوریل",
+ "می",
+ "جون",
+ "جولای",
+ "آگوست",
+ "سپتامبر",
+ "اکتبر",
+ "نوامبر",
+ "دسامبر");
+
+Calendar._SMN = new Array
+("Jan",
+ "Feb",
+ "Mar",
+ "Apr",
+ "May",
+ "Jun",
+ "Jul",
+ "Aug",
+ "Sep",
+ "Oct",
+ "Nov",
+ "Dec");
+
+Calendar._JMN = new Array
+("فروردین",
+ "اردیبهشت",
+ "خرداد",
+ "تیر",
+ "مرداد",
+ "شهریور",
+ "مهر",
+ "آبان",
+ "آذر",
+ "دی",
+ "بهمن",
+ "اسفند");
+
+Calendar._JSMN = new Array
+("فروردین",
+ "اردیبهشت",
+ "خرداد",
+ "تیر",
+ "مرداد",
+ "شهریور",
+ "مهر",
+ "آبان",
+ "آذر",
+ "دی",
+ "بهمن",
+ "اسفند");
+
+Calendar._TT = {};
+Calendar._TT["PREV_YEAR"] = "سال قبل (hold for menu)";
+Calendar._TT["PREV_MONTH"] = "ماه قبل (hold for menu)";
+Calendar._TT["GO_TODAY"] = "رفتن به امروز";
+Calendar._TT["NEXT_MONTH"] = "ماه بعد (hold for menu)";
+Calendar._TT["NEXT_YEAR"] = "سال بعد (hold for menu)";
+Calendar._TT["SEL_DATE"] = "انتخاب تاریخ";
+Calendar._TT["DRAG_TO_MOVE"] = "Drag to move";
+Calendar._TT["PART_TODAY"] = " (امروز)";
+
+Calendar._TT["DAY_FIRST"] = "ابتدا %s نمایش داده شود";
+
+Calendar._TT["SELECT_COLUMN"] = "انتخاب تمام %s‌های این ماه";
+Calendar._TT["SELECT_ROW"] = "انتخاب روزهای این هفته";
+
+Calendar._TT["WEEKEND"] = "5";
+
+Calendar._TT["CLOSE"] = "بستن";
+Calendar._TT["TODAY"] = "امروز";
+Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
+
+Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
+Calendar._TT["TT_DATE_FORMAT"] = "%A, %e %b";
+
+Calendar._TT["WK"] = "هفته";
+Calendar._TT["TIME"] = "زمان :";
+
+Calendar._TT["LAM"] = "ق.ظ.";
+Calendar._TT["AM"] = "ق.ظ.";
+Calendar._TT["LPM"] = "ب.ظ.";
+Calendar._TT["PM"] = "ب.ظ.";
+
+Calendar._NUMBERS = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+Calendar._DIR = 'rtl';
